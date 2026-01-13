6842026a13e709b51f22304bb9aaf0f3fddcb2ea
alavuvi <ville-pekka.alavuotunki@proton.me>
2026-01-13 12:32:29 +0200

    started on bottom nav

--- MobileApp/App.tsx
+++ MobileApp/App.tsx
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useNavigation, NavigationContainer } from "@react-navigation/native";
+import BottomNav from "./components/BottomNav";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <NavigationContainer>
+        <BottomNav />
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
