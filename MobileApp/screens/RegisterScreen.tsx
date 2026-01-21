63dcfee523da82beaed669c464fe8c4cbba4a9a3
Kata <kata.niva03@gmail.com>
2026-01-21 12:28:25 +0200

    background color bug fixed

--- MobileApp/screens/RegisterScreen.tsx
+++ MobileApp/screens/RegisterScreen.tsx
@@ -50,6 +50,7 @@ export default function RegisterScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: 'white'
     },
     title: {
         fontSize: 36,
