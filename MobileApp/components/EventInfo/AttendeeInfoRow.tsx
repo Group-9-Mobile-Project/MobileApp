03557d7c98d036fcc274ce5a96be99d61f1bfbfa
TeroAsilainen <tero_asilainen@hotmail.com>
2026-02-03 17:46:48 +0200

    Attendees Info

--- MobileApp/components/EventInfo/AttendeeInfoRow.tsx
+++ MobileApp/components/EventInfo/AttendeeInfoRow.tsx
@@ -1,18 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Touchable, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Event } from '../../types/Event'
 import { UserInfo } from '../../types/UserInfo'
 import { doc, onSnapshot } from 'firebase/firestore'
 import { firestore, USERINFO } from '../../firebase/Config'
 import { Divider } from 'react-native-paper'
+import AttendeeInfoModal from './AttendeeInfoModal'
 
 interface AttendeeInfoProps {
+
     email: string
 }
 
 export default function AttendeeInfoRow({ email }: AttendeeInfoProps) {
     const [attendee, setAttendee] = useState<UserInfo | null>(null)
     const [loading, setLoading] = useState<boolean>(true)
+    const [showModal, setShowModal] = useState<boolean>(false)
 
     useEffect(() => {
         const docRef = doc(firestore, USERINFO, email)
@@ -29,20 +32,23 @@ export default function AttendeeInfoRow({ email }: AttendeeInfoProps) {
 
     if (!loading && attendee) {
         return (
-        <View>
-            <Text style={styles.subHeading}>{attendee.name}</Text>
-            <Divider bold={true}/>
-        </View>
-    )
+            <View style={{width: '100%'}}>
+                <Pressable onPress={() => setShowModal(true)} >
+                    <Text style={styles.subHeading}>{attendee.name}</Text>
+                    <Divider bold={true} />
+                    <AttendeeInfoModal showModal={showModal} setShowModal={setShowModal} attendee={attendee} />
+                </Pressable>
+            </View>
+        )
     }
-    
+
 }
 
 const styles = StyleSheet.create({
-  subHeading: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    padding: 5
-  },
-  
+    subHeading: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        padding: 5
+    },
+
 })
