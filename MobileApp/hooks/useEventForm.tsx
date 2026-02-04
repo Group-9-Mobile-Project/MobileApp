b6d6363d99b7eaab5ad5ca978ffcd0b232093671
villepekkaa <ville-pekka.alavuotunki@proton.me>
2026-02-04 14:45:25 +0200

    fixed #56 in updateEvent functionality

--- MobileApp/hooks/useEventForm.tsx
+++ MobileApp/hooks/useEventForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { EventType } from "../types/Event";
 
 type Coordinate = { latitude: number; longitude: number };
