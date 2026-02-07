55dddb8f8861c0a6c563b97dfd5d1ab392c65705
TeroAsilainen <tero_asilainen@hotmail.com>
2026-02-07 11:42:05 +0200

    eventStartingBanner visibility probably works

--- MobileApp/screens/HomeScreen.tsx
+++ MobileApp/screens/HomeScreen.tsx
@@ -3,12 +3,15 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import RecentEventsList from '../components/Home/RecentEventsList';
 import AllEventsMapView from '../components/Home/AllEventsMapView';
 import EventSearchModal from '../components/Home/EventSearchModal';
+import EventStartingBanner from '../components/Home/EventStartingBanner';
 
 export default function HomeScreen() {
   return (
 
     <ScrollView contentContainerStyle={styles.scrollview}>
       <View style={styles.container}>
+        <EventStartingBanner />
+        
         <RecentEventsList />
         
         <AllEventsMapView />
